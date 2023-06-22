04ccf7af79343e57eaf7c617fcf3b55151b2a20e
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-06-23 03:28:08 +0700

    update spawning logic

--- Assets/_Main/Scripts/Sceners/StoryGameScener.cs
+++ Assets/_Main/Scripts/Sceners/StoryGameScener.cs
@@ -1,15 +1,35 @@
+using System.Collections;
+using System.Threading.Tasks;
 using FishNet;
+using FishNet.Object;
 using UnityEngine;
 
-public class StoryGameScener : MonoBehaviour
+public class StoryGameScener : NetworkBehaviour
 {
     [SerializeField] Transform _spawnPoint;
 
-    Player _player;
+    GameObject _player;
 
-    private void Start()
+    public override async void OnStartClient()
     {
-        _player = Instantiate(StoryGameManager.Instance.PlayerPrefab, _spawnPoint.position, Quaternion.identity);
-        InstanceFinder.ServerManager.Spawn(_player.gameObject);
+        base.OnStartClient();
+
+        var waitTime = 3f;
+        const float checkTime = 0.5f;
+        while (!base.IsOwner && waitTime > 0f)
+        {
+            waitTime -= checkTime;
+            await Task.Delay((int)(checkTime * 1000));
+        }
+
+        if (base.IsOwner)
+        {
+            _player = Instantiate(StoryGameManager.Instance.PlayerPrefab, _spawnPoint.position, Quaternion.identity);
+            ServerManager.Spawn(_player, ClientManager.Connection);
+        }
+        else
+        {
+            Debug.LogError(base.IsOwner);
+        }
     }
 }

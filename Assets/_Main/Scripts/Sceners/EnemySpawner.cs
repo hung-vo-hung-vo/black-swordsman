889dc6b62889892f92e3eee59ff637ea5c2b4d52
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-07-29 09:54:16 +0700

    impl spawn func and apply to Forest scene

--- Assets/_Main/Scripts/Sceners/EnemySpawner.cs
+++ Assets/_Main/Scripts/Sceners/EnemySpawner.cs
@@ -1,6 +1,27 @@
+using FishNet;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] SpawnEnemy[] _enemies;
+
+    private void Start()
+    {
+        foreach (var pE in _enemies)
+        {
+            if (pE.prefab == null)
+            {
+                continue;
+            }
+
+            foreach (var p in pE.spawnPoints)
+            {
+                if (p != null)
+                {
+                    var e = Instantiate(pE.prefab.gameObject, p.position, p.rotation);
+                    InstanceFinder.ServerManager.Spawn(e);
+                }
+            }
+        }
+    }
 }

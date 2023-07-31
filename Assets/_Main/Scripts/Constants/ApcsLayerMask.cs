becdd788a14ea9d03db234c5c019b0685803f221
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-08-01 01:11:42 +0700

    dead on falling

--- Assets/_Main/Scripts/Constants/ApcsLayerMask.cs
+++ Assets/_Main/Scripts/Constants/ApcsLayerMask.cs
@@ -3,4 +3,5 @@ public class ApcsLayerMask
     public const string FOREGROUND = "Foreground";
     public const string DAMAGEABLE_ENEMY = "Damageable";
     public const string PICKABLE_ITEM = "Pickable";
+    public const string DEATH = "Death";
 }
